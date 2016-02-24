ffe584928616607be9685e1df4437a9715ce68be
Lipu Fei <lipu.fei815@gmail.com>
2016-02-24 14:10:38 +0100

    Enable passing password through env variable

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 from ConfigParser import ConfigParser
 import logging
 import getpass
+import os
 
 from bot.bot import Bot
 
@@ -15,9 +16,12 @@ if __name__ == '__main__':
     config = ConfigParser()
     config.read([config_file])
 
-    # get password
-    print u"Please input your Hipchat password:"
-    password = getpass.getpass()
+    password = os.getenv(u'HCBOT_PASSWORD', u'').strip()
+
+    # get password if it's not passed through the environment variable
+    if password:
+        print u"Please input your Hipchat password:"
+        password = getpass.getpass()
 
     bot = Bot(config_file, config, password)
     bot.start()
